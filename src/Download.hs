130dbf5df24a74b5fa8c42c1f7db51f077049b70
jeorp <jeorpqxx@gmail.com>
2021-12-25 21:18:26 +0900

    upd

--- src/Download.hs
+++ src/Download.hs
@@ -2,9 +2,39 @@
 
 module Download where
 import Network.HTTP.Simple
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BL
+import System.IO
+import System.Directory
+import Data.Strings
 
-downloadPic :: Request -> IO ()
+
+downloadPic :: String -> IO ()
 downloadPic url = do
-  res <- httpLBS url
-  print (getResponseBody res)
-  print (getResponseHeaders res)
+  let outFile = "temp/" ++ urlToFileName url
+  fileExist <- doesFileExist outFile
+  if fileExist
+    then return ()
+    else do 
+      res <- httpLBS $ parseRequest_ url
+      let xs = getResponseHeader "Content-Type" res
+          file = getResponseBody res
+          contentType = if not (null xs) then head xs else ""
+      print contentType
+      store outFile file contentType
+  where
+    urlToFileName :: String -> String
+    urlToFileName s = 
+      let xs = strSplitAll "/" s
+          ls = if null xs then "error" else last xs
+          in ls 
+    store :: FilePath -> BL.ByteString -> B.ByteString -> IO ()
+    store path bs c = do
+      if B.isInfixOf "image/" c 
+        then do 
+          let picType = BS.unpack $ snd $ B.splitAt (B.length "image/") c 
+          fin <- openBinaryFile (path<>picType) WriteMode
+          hPutStr fin (BL.unpack bs)
+          hClose fin
+        else putStrLn "not picture file"
--- test/DownloadSpec.hs
+++ test/DownloadSpec.hs
@@ -2,11 +2,13 @@ module DownloadSpec where
 
 import Test.Hspec
 
+import Download
+
 main :: IO () 
 main = hspec spec
 
 spec :: Spec
 spec = do
   describe "Test Download picture" $ do
-    it "" $ do
-      pure "" `shouldReturn` "" 
+    it "test 1" $ do
+      downloadPic "https://pbs.twimg.com/media/FHcf4AKaIAk5soi?format=jpg&name=large" `shouldReturn` ()
