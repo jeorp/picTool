49bb4676512b49b23fb43b3c3d420b3ba8a32416
jeorp <jeorpqxx@gmail.com>
2021-12-25 22:08:29 +0900

    update Download.hs

--- src/Download.hs
+++ src/Download.hs
@@ -4,7 +4,6 @@ module Download where
 import Network.HTTP.Simple
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.Lazy.Char8 as BL
 import System.IO
 import System.Directory
 import Data.Strings
@@ -13,24 +12,20 @@ import Data.Strings
 downloadPic :: String -> String -> IO ()
 downloadPic url path = do
   let outFile = "temp/" ++ path
-  fileExist <- doesFileExist outFile
-  if fileExist
-    then return ()
-    else do 
-      res <- httpLBS $ parseRequest_ url
-      let xs = getResponseHeader "Content-Type" res
-          file = getResponseBody res
-          contentType = if not (null xs) then head xs else ""
-      print contentType
-      store outFile file contentType
+  res <- httpBS $ parseRequest_ url
+  let xs = getResponseHeader "Content-Type" res
+      file = getResponseBody res
+      contentType = if not (null xs) then head xs else ""
+  print contentType
+  store outFile file contentType
   where
-    store :: FilePath -> BL.ByteString -> B.ByteString -> IO ()
+    store :: FilePath -> B.ByteString -> B.ByteString -> IO ()
     store path bs c = do
       if B.isInfixOf "image/" c 
         then do 
           let picType = BS.unpack $ snd $ B.splitAt (B.length "image/") c 
           fin <- openBinaryFile (path<>"."<>picType) WriteMode
-          hPutStr fin (BL.unpack bs)
+          hPutStr fin (BS.unpack bs)
           hClose fin
         else putStrLn "not picture file"
 
